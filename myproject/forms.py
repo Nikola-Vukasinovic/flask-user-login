75e789c67768a47c3d651ac8231307b537d858b4
NikolaVukashinovich <nikola.vukasinovic@spotsie.io>
2024-07-24 15:56:56 +0200

    added project files

---
+++ myproject/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms import ValidationError
+from myproject.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords Must Match!')])
+    pass_confirm = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Register!')
+
+    def check_email(self,email):
+        if User.query.filter_by(email=self.email.data).first():
+            raise ValidationError('Your email has already been registered!')
+ 
+    def check_username(self,username):
+        if User.query.filter_by(username=self.username.data).first():
+            raise ValidationError('Username is taken!')
